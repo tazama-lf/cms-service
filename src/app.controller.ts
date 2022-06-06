5cca1a09a28073e8beab0f53f9829841ac2c2290
JFoley <johannes.foley@sybrin.com>
2022-06-06 11:12:28 +0200

    Implement logging for Interdiction from TP

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,11 @@ import { LoggerService } from './utils';
 export const monitorQuote = async (ctx: Context): Promise<Context> => {
   try {
     const request = ctx.request.body ?? JSON.parse('');
+    if (request.typologyResult)
+      LoggerService.log(`CMS received Interdiction from Typology ${request?.typologyResult?.id ?? 0}@${request?.typologyResult?.cfg ?? 0} - Score: ${request?.typologyResult?.result ?? 0}, Threshold: ${request?.typologyResult?.threshold ?? 0}.`);
+    else
+      LoggerService.log('CMS received request from TADP.');
 
-    LoggerService.log('CMS received request from TADP.');
     ctx.status = 200;
     ctx.body = {
       message: 'Transaction is valid',
